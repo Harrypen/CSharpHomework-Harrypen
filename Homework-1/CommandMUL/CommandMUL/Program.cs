54e2e6462a048e9041ab21280e756ff662393bbb
Harrypan <756232143@qq.com>
2018-09-09 19:18:07 +0800

    第一次作业: CommandMUL为第四题, WindowsMUL为第五题

---
+++ Homework-1/CommandMUL/CommandMUL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommandMUL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个整数");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("乘积为:"+num1*num2);
+            Console.ReadKey();
+        }
+    }
+}
